0af293f422181502a9d2bdd8c742fe629228f0fe
donax96 <62355624+donax96@users.noreply.github.com>
2020-05-31 18:25:59 +0200

    Add files via upload

--- include/trackingImages.h
+++ include/trackingImages.h
@@ -11,10 +11,19 @@ public:
 
 	void locate_objects(cv::String object_paths);
 
+	void track_motion();
+
+	void save_video(cv::String path);
+
 protected:
 
 	std::vector<cv::Mat> frames, objects, equalized_objects;
 	std::vector<cv::Mat> obj_descriptors, frames_descriptors;
 	std::vector<std::vector<cv::KeyPoint>> obj_keypoints, frames_keypoints;
+	std::vector<std::vector<cv::Point2f>> best_matches, obj_corners;
+	std::vector<cv::Scalar> colors;
+	
+private:
+	int fourcc;
 
 };
